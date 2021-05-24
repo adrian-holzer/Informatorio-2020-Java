74ff8f5dcf09b84ec1b8147d42ba5057cbcbb3c5
Adrian Holzer <adri.holzer.e@gmail.com>
2021-05-24 18:17:33 -0300

    Ejercicios

---
+++ Complementarios-1/Ejercicio_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/* 
+Solicitar por consola el nombre del usuario e imprimir por pantalla el siguiente mensaje “HOLA {USUARIO}!!!”
+
+*/
+public class Ejercicio_1 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+ 
+        System.out.println("Ingresar rango  : ");
+        int str = scan.nextInt();
+
+        String arr[] = new String[str];
+
+
+        
+        
+        System.out.println("hola mundo");
+    }
+}
---
+++ Complementarios-1/Ejercicio_2.java
@@ -0,0 +1,9 @@
+public class Ejercicio_2 {
+    
+    public static void main(String[] args) {
+
+        int a = 3 ; 
+
+        System.out.println("ejercicio 2" + a );
+    }
+}
